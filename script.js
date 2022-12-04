b380d0a6c24fd4e2b683bcf9c4ee520fa3514536
Ugurismail <uguraygun001@hotmail.com>
2022-12-04 22:35:30 +0300

    backend Ok

--- script.js
+++ script.js
@@ -1,84 +1,135 @@
 "use strict";
-
-const adayInput = document.getElementById("adayInput");
-const adayForm = document.getElementById("adayAdet");
-const adayKistasContainer = document.querySelector(".adayadForm");
-const adayKistas = document.querySelector(".adayadContainer");
-
-adayForm.addEventListener("submit", (e) => {
+let adaylar = [];
+let kriterler = [];
+let adayAdet = "";
+document.getElementById("adayAdetForm").addEventListener("submit", (e) => {
 	e.preventDefault();
+	adayAdet = document.getElementById("adayAdetNum").value;
 
-	for (let index = 0; index < adayInput.value; index++) {
-		// const element = array[index];
-		const eklenecek = `
-
-    <label for="adayad">${index + 1} No.lu Adayın adı</label>
-    <input id="adayad" type="text">
-    `;
-		adayKistasContainer.insertAdjacentHTML("beforebegin", eklenecek);
+	for (let index = 0; index < adayAdet; index++) {
+		document.getElementById("adayNameBtn").insertAdjacentHTML(
+			"beforebegin",
+			`
+				<label for="adayName"> ${index + 1} numaralı adayın ismi </label>
+				<input id="adayNameId" class="adayName" type="text">
+			`,
+		);
 	}
-
-	document.querySelector(".adayadBtn").style.display = "block";
-	document.querySelector(".adayAdetContainer").style.display = "none";
+	document.getElementById("adayNameBtn").classList.remove("hidden");
 });
 
-const adayAdForm = document.querySelector(".adayadForm");
-
-let adayisimleri;
-adayAdForm.addEventListener("submit", (e) => {
+document.getElementById("adayNameForm").addEventListener("submit", (e) => {
 	e.preventDefault();
-	adayisimleri = document.querySelectorAll("#adayad");
-	// adayisimleri.forEach((aday) => console.log(aday.value));
-	document.querySelector(".adayadContainer").style.display = "none";
-	document.querySelector(".adayOzellikContainer").style.display = "block";
-});
 
-const adayOzellikForm = document.querySelector("#adayOzellikForm");
-
-const adayDegerleriForm = document.querySelector(".adayDegerleriForm");
-adayOzellikForm.addEventListener("submit", (e) => {
-	e.preventDefault();
-	document.querySelector(".adayOzellikContainer").style.display = "none";
-	const ozellikAdet = document.querySelector("#adayOzellikAdet").value;
-	for (let index = 0; index < ozellikAdet; index++) {
-		// const element = array[index];
-		const eklenecekOzellikler = `
-    <label for="adayOzellik">${index + 1} No.lu Aday Ozelliği</label>
-    <input id="adayOzellik" type="text">
-    <label for="adayOzellikDeger">Bu Özelliğin %'olarak değeri</label>
-    <input id="adayOzellikDeger" type="number">
-    `;
-
-		adayDegerleriForm.insertAdjacentHTML("beforebegin", eklenecekOzellikler);
-		document.querySelector(".adayDegerleriBtn").style.display = "block";
-	}
-});
-
-adayDegerleriForm.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const adayOzellikler = document.querySelectorAll("#adayOzellik");
-	const adayOzellikDegerler = document.querySelectorAll("#adayOzellikDeger");
-
-	adayisimleri.forEach((aday) => {
-		const sonucForm = document.querySelector(".sonucForm");
-
-		adayOzellikler.forEach((ozellik) => {
-			const eklenecekSonuclar = `
-      <label for="sonuc">${aday.value} Adayının ${ozellik.value} Özeliğine beğeniniz % ?</label>
-      <input id="sonuc" type="number">
-      `;
-
-			sonucForm.insertAdjacentHTML("beforebegin", eklenecekSonuclar);
-		});
+	let a = 0;
+	document.querySelectorAll(".adayName").forEach((aday) => {
+		adaylar[a] = {
+			adı: `${aday.value}`,
+			puanlar: [],
+		};
+		a++;
 	});
-	const x = document.querySelector(".adayDegerleriContainer");
-	x.style.display = "none";
 
-	document.querySelector(".sonucBtn").style.display = "block";
-	// adayOzellikler.forEach((ozellik) => {
-	// 	console.log(ozellik.value);
-	// });
-	// adayOzellikDegerler.forEach((ozellikDeg) => {
-	// 	console.log(ozellikDeg.value);
-	// });
+	document.getElementById("ozAdet").classList.remove("hidden");
+});
+
+document.getElementById("ozAdetForm").addEventListener("submit", (e) => {
+	e.preventDefault();
+	document.getElementById("adayOzellik").classList.remove("hidden");
+
+	for (
+		let index = 0;
+		index < document.getElementById("ozAdetNum").value;
+		index++
+	) {
+		document.getElementById("adayOzBtn").insertAdjacentHTML(
+			"beforebegin",
+			`
+			<label for="ozName">${index + 1} numaralı Ozelliğin Adı</label>
+			<input id="ozName" type="text">
+		`,
+		);
+	}
+});
+
+document.getElementById("adayOzForm").addEventListener("submit", (e) => {
+	e.preventDefault();
+	let b = 0;
+	document.querySelectorAll("#ozName").forEach((ozellik) => {
+		kriterler[b] = {
+			kriter: `${ozellik.value}`,
+			kriterDgr: "",
+		};
+		b++;
+
+		document.getElementById("OzBtn").insertAdjacentHTML(
+			"beforebegin",
+			`
+		<label for="ozPuan">${ozellik.value} Özelliği % kaç önemli sizin için?</label>
+		<input id="ozPuan" class="ozPuan" type="number">
+		`,
+		);
+	});
+
+	document.getElementById("OzBtn").classList.remove("hidden");
+});
+
+document.getElementById("ozellikDegerler").addEventListener("submit", (e) => {
+	e.preventDefault();
+	document.getElementById("sonDegerlemeBtn").classList.remove("hidden");
+	let c = 0;
+	document.querySelectorAll(".ozPuan").forEach((puan) => {
+		kriterler[c].kriterDgr = puan.value;
+		c++;
+	});
+
+	let d = 0;
+	for (const aday of adaylar) {
+		for (const kriter of kriterler) {
+			document.getElementById("sonDegerlemeBtn").insertAdjacentHTML(
+				"beforebegin",
+				`
+			<label for="SnDgr">${aday.adı} adayının ${kriter.kriter} özelliğine 1 ila 10 arasında bir değer verin.</label>
+			<input id="SnDgr" type="number">
+			`,
+			);
+		}
+		d++;
+	}
+});
+
+let nihayetList = [];
+let neticeList = [];
+document.getElementById("sonDegerlemeForm").addEventListener("submit", (e) => {
+	e.preventDefault();
+	const dgrAdet = document.querySelectorAll("#SnDgr").length;
+	const dgrlerList = [];
+	document.querySelectorAll("#SnDgr").forEach((puan) => {
+		dgrlerList.push(puan.value);
+	});
+	for (let i = 0; i < adaylar.length; i++) {
+		const kesilenPuanlar = dgrlerList.splice(0, kriterler.length);
+
+		adaylar[i].puanlar.push(kesilenPuanlar);
+	}
+	for (let i = 0; i < kriterler.length; i++) {
+		adaylar.forEach((aday) => {
+			aday.puanlar.forEach((puan) => {
+				neticeList.push(puan[i] * kriterler[i].kriterDgr);
+			});
+		});
+	}
+
+	console.log(adaylar, kriterler, neticeList);
+
+	function adayPuanHesaplama(adaylar, kriterler, neticeList) {
+		for (let i = 0; i < adaylar.length; i++) {
+			let netice = 0;
+			for (let k = i; k < neticeList.length; k += adaylar.length) {
+				netice += neticeList[k];
+			}
+			console.log(`${adaylar[i].adı} adayının toplam puanı ${netice}`);
+		}
+	}
+	adayPuanHesaplama(adaylar, kriterler, neticeList);
 });
